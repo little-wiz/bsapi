7a36b33b72497212c2caf7f8d9f69ad6cb001cf4
little-wiz <lishingchun2020@gmail.com>
2021-11-19 16:31:45 +0800

    -

---
+++ src/com/wizard/brawlstars/util/API.java
@@ -0,0 +1,5 @@
+package com.wizard.brawlstars.util;
+
+public class API {
+
+}
---
+++ src/com/wizard/brawlstars/util/BrawlStars.java
@@ -0,0 +1,17 @@
+package com.wizard.brawlstars.util;
+
+public class BrawlStars {
+	private String token;
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	public String get() {
+		
+	}
+}
